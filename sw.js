738ccf78ed81da883e5356865e4b64756fda82c1
compukelc-tech <compukelc@gmail.com>
2026-09-18 20:36:15 -0500

    Update cache name from v2 to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'estanco-em-v2';
+const CACHE_NAME = 'estanco-em-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -13,7 +13,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Archivos cacheados exitosamente (v2)');
+        console.log('Archivos cacheados exitosamente (v4)');
         return cache.addAll(urlsToCache);
       })
   );
